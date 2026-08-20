b3149923f959dee41e5bb4327b0027f442f13608
Posselinha <gabrielfilipeposs@hotmail.com>
2026-08-19 21:57:09 -0300

    Commit inicial de projeto

---
+++ src/siga/Professor.java
@@ -0,0 +1,16 @@
+package siga;
+
+/**
+ * Código INICIAL da disciplina.
+ *
+ * Observe que Aluno e Professor repetem os atributos "nome" e "matricula"/"registro".
+ * Na atividade da Aula 1, essa duplicação será eliminada com a criação de uma
+ * superclasse comum (Pessoa), aplicando herança.
+ */
+public class Professor {
+
+    // Atributos públicos: mesma violação de encapsulamento da classe Aluno.
+    public String nome;
+    public String siape;
+    public boolean ativo;
+}
